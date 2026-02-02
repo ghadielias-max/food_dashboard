7e2c20b52e598328fbc2c4a2086b46d610de2093
BAGGADI <gh8adi_elias@outlook.com>
2026-02-02 10:36:50 +0300

    nothing

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -16,7 +16,7 @@ export default function SettingsPage() {
           Settings
         </h2>
         <p className="text-muted mt-1">
-          Customize your dashboard appearance and business detailss
+          Customize your dashboard appearance and business details
         </p>
       </div>
 
